2b338aea2504fb3be87eaf87d523443cf578dd8b
rperssuf <e.emi24@info.uas.edu.mx>
2025-11-23 18:29:51 -0700

    Add files via upload

---
+++ Galeria.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Galeria(): JSX.Element {
+  return (
+    <div>
+      <h1>Galería de Spamton</h1>
+      <p>Aquí se muestran las imagenes sobre spamton.</p>
+    </div>  
+
+
+  )
+
+}
---
+++ Historia.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Historia(): JSX.Element {
+  return (
+    <div>
+      <h1>Historia de Spamton</h1>
+      <p>Spamton es un personaje de Deltarune que me gusta :v</p>
+    </div>
+  )
+}
